31b23193d04d14cc9c5fe34a265d5b76a866f38c
Loreen-netizen <loreenmuusha@gmail.com>
2021-08-05 15:33:57 +0200

    display image added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,40 @@
-import {Container, Grid} from "@material-ui/core";
+import { StylesProvider } from "@material-ui/core/styles";
+import { Container, Grid } from "@material-ui/core";
 
 import SideBarProfile from "../SideBarProfile";
 import Header from "../Header";
-import  Portfolio from "../../views/Portfolio";
+import Portfolio from "../../views/Portfolio";
 import Resume from "../../views/Resume";
 import Footer from "../Footer";
 
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export const App = () =>{
+export const App = () => {
   return (
-    <Container>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={4} lg={3}>
-          <SideBarProfile />
+    <StylesProvider injectFirst>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={4} lg={3}>
+            <SideBarProfile />
+          </Grid>
+          <Grid item xs>
+            <Header />
+            <Router>
+              <Switch>
+                <Route path="/resume">
+                  <Resume />
+                </Route>
+                <Route path="/">
+                  <Portfolio />
+                </Route>
+              </Switch>
+            </Router>
+            <Footer />
+          </Grid>
         </Grid>
-        <Grid item xs>
-          <Header />
-          <Router>
-            <Switch>
-              <Route path="/resume">
-                <Resume />
-              </Route>
-              <Route path="/">
-                <Portfolio />
-              </Route>
-            </Switch>
-          </Router>
-          <Footer />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </StylesProvider>
   );
-}
+};
 
 export default App;
--- src/components/SideBarProfile/SideBarProfile.jsx
+++ src/components/SideBarProfile/SideBarProfile.jsx
@@ -1,8 +1,11 @@
 import { Typography } from '@material-ui/core'
 import React from 'react';
 
-import tokens from "../../data/tokens"
-import styled from "styled-components"
+import tokens from "../../data/tokens";
+import styled from "styled-components";
+import ProfileCss from "./Profile.css";
+import CustomTimeLine from "../Timeline/TimeLine"
+
 
 const ProfileDiv = styled.div`
 display: inline-block;
@@ -16,29 +19,30 @@ const ProfileName = styled.div`
 `;
 
 const StyledTypography = styled(Typography)`
-  && {
-    font-size: ${tokens.text.m.size};
-    font-weight: ${tokens.text.m.weight};
-    font-spacing: ${tokens.text.m.spacing};
-    font-height: ${tokens.text.m.height};
-    color: ${tokens.colors.vividSkyBlue} ${tokens.opacity.medium};
-  }
+  font-size: ${tokens.text.m.size};
+  font-weight: ${tokens.text.m.weight};
+  font-spacing: ${tokens.text.m.spacing};
+  line-height: ${tokens.text.m.height};
+  color: ${tokens.colors.black} ${tokens.opacity.medium};
 `;
 
 export const SideBarProfile = () => {
     return (
       <ProfileDiv>
         <ProfileName>
-          <StyledTypography>YEU</StyledTypography>
-          <StyledTypography>title</StyledTypography>
+          <Typography className="name">YEU</Typography>
+          <Typography className="title">title</Typography>
         </ProfileName>
 
         <figure className="profile_image">
-          <img src="" alt=""></img>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/Yeukai.LoreenProfesionalPhoto.jpg`}
+            alt="YeukaiLoreenProfesionalPhoto"
+          ></img>
         </figure>
 
         <div className="profile_information">
-          TIMELINE
+          <CustomTimeLine/>
           <br />
           <button>click me!</button>
         </div>
