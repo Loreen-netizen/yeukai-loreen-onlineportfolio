e1476534df3dc62ebd7f7b264f304ca7671bece3
Loreen-netizen <loreenmuusha@gmail.com>
2021-08-06 21:17:03 +0200

    navbar setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,44 +2,100 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav, Navbar } from "react-bootstrap";
 // import {BrowserRouter as Router} from "react-router-dom";
-import { HomeRounded } from "@material-ui/icons";
+import { HomeRounded, Telegram } from "@material-ui/icons";
+import { RESUME_DATA } from "../../utils/resumeData";
+import CustomButton from "../../components/CustomButton";
+import { withRouter } from "react-router";
+import uuid from "react-uuid";
+import tokens from "../../data/tokens"
+
+import styled from "styled-components";
+
+const StyledNavbar = styled(Navbar)`
+  && {
+    margin-bottom: 30px;
+    background-color:white;
+    padding:0 22px 0 0;
+    border-radius: 6px;
+    box-shadow: 0px 2px 92px 0px rgba(0, 0, 0, 0.07)
+  }`;
+
+  const StyledNavLink = styled(Nav.Link)`
+    && {
+      font-weight: 500;
+      color: darkslategrey;
+      // &:hover {
+      //   background: ${tokens.colors.flickrPink};
+      // }
+      // &:active {
+      //   background: ${tokens.colors.flickrPink};
+      // }
+    }
+  `;
+
+
+
 
 export const Header = (props) => {
   const pathName = props?.location?.pathname;
   return (
-    <Navbar expand="lg" sticky="top">
+    <StyledNavbar expand="lg" sticky="top">
       {/* Home link */}
-      <Nav.Link to="/">
+      <StyledNavLink href="/">
         <Navbar.Brand>
           <HomeRounded />
         </Navbar.Brand>
-      </Nav.Link>
+      </StyledNavLink>
 
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
           {/* Resume link */}
-          <Nav.Link to="/" active={pathName === "/" ? true : false}>
-           Resume
-          </Nav.Link>
+          <StyledNavLink href="/" active={pathName === "/" ? true : false}>
+            Resume
+          </StyledNavLink>
           {/* Porfolio link */}
-          <Nav.Link to="/portfolio" active={pathName === "/" ? true : false}>
-          Portfolio
-          </Nav.Link>
+          <StyledNavLink
+            href="/portfolio"
+            active={pathName === "/portfolio" ? true : false}
+          >
+            Portfolio
+          </StyledNavLink>
           {/* Skills */}
-          <Nav.Link to="/skills" active={pathName === "/" ? true : false}>
+          <StyledNavLink
+            href="/skills"
+            active={pathName === "/skills" ? true : false}
+          >
             Skills
-          </Nav.Link>
+          </StyledNavLink>
 
           {/* Experience */}
-          <Nav.Link to="/Experience" active={pathName === "/" ? true : false}>
+          <StyledNavLink
+            href="/experience"
+            active={pathName === "/experience" ? true : false}
+          >
             Experience
-          </Nav.Link>
+          </StyledNavLink>
         </Nav>
+        <div>
+          {Object.keys(RESUME_DATA.socials).map((socialMedia) => {
+            return (
+              <a
+                href={RESUME_DATA.socials[socialMedia].link}
+                alt={RESUME_DATA.socials[socialMedia].text}
+                target="_blank"
+                key={uuid()}
+              >
+                {RESUME_DATA.socials[socialMedia].icon}
+              </a>
+            );
+          })}
+          <CustomButton text="Hire Me" icon={<Telegram />} />
+        </div>
       </Navbar.Collapse>
       {/* </Navbar.Toggle> */}
-    </Navbar>
+    </StyledNavbar>
   );
 };
 
-export default Header;
+export default withRouter(Header);
